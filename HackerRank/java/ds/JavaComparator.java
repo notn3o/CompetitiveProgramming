0259c8a2df635102633fc49c2eadbd7f0a69502a
Py <pyjcpp@gmail.com>
2018-03-03 04:23:21 -0600

    Add missing property in comparator implementation logic

--- HackerRank/java/ds/JavaComparator.java
+++ HackerRank/java/ds/JavaComparator.java
@@ -15,7 +15,7 @@ class Player {
 class Checker implements Comparator<Player> {
     @Override
     public int compare(Player p1, Player p2) {
-        if (p1 == p2) {
+        if (p1.score == p2.score) {
             return p1.name.compareTo(p2.name);  
         }
         return p2.score - p1.score;
